a645e41739e401fb67ce7349a4904e405bba22fe
Malmeu <darkvid3r@gmail.com>
2026-05-12 15:57:16 +0100

    fix: force SSR mode and add custom 404 to resolve Vercel routing issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  output: 'server', // Passer en mode serveur pour corriger les problèmes de routage sur Vercel
+  build: {
+    format: 'directory'
+  }
 });
